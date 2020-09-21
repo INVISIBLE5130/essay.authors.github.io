d1af941a3306be6180bebfc1290ac29f42e451ff
INVISIBLE5130 <goshakovan14@gmail.com>
2020-09-21 12:33:27 +0300

    Files were renamed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 document.querySelectorAll('.lena').forEach(item => {
     item.addEventListener('click', () => {
-        window.location.assign('authors.html')
+        window.location.assign('author.html')
     })
 })
 
 document.querySelectorAll('.essays_main-right_article-title').forEach(item => {
     item.addEventListener('click', () => {
-        window.location.assign('author.html')
+        window.location.assign('article.html')
     })
 })
 
 document.querySelectorAll('.articles').forEach(item => {
     item.addEventListener('click', () => {
-        window.location.assign('authors.html')
+        window.location.assign('author.html')
     })
 })
